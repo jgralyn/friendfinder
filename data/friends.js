bea2a574ff5c63545284a3beca6171310d185b91
jgralyn <44208783+jgralyn@users.noreply.github.com>
2019-05-12 23:19:23 -0400

    Add files via upload

---
+++ data/friends.js
@@ -0,0 +1,4 @@
+var friends = [{ "name": "Micheal Jordan", "photo": "img src="images/MJ.png)", "scores": ["5", "1", "4", "4", "5", "1", "2", "5", "4", "1"] }, { "name": "Charles Barkley", "photo": "img src="images/charles.jpg")", "scores": ["4", "2", "5", "1", "3", "2", "2", "1", "3", "2"] }, { "name": "Lebron James", "photo": "img src="images/Lebron.png)", "scores": ["5", "2", "2", "2", "4", "1", "3", "2", "5", "5"] }, { "name": "Shaquil O'Neal", "photo": "img src="images/shaq.jpg)", "scores": ["3", "3", "4", "2", "2", "1", "3", "2", "2", "3"] }, { "name": "Kobe Bryant", "photo": "img src="images/kobe.png)", "scores": ["4", "3", "4", "1", "5", "2", "5", "3", "1", "4"] }, { "name": "Carmelo Anthony", "photo":"img src="images/carmelo.jpg)", "scores": ["4", "4", "2", "3", "2", "2", "3", "2", "4", "5"] }];
+
+// Export the data
+module.exports = friends;
